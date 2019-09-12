4726b12f37d9b84220249eb5329b488f5ea50223
Ivelin Ivanov <ivelin117@gmail.com>
2019-09-12 16:39:48 -0500

    Update install_requirements.sh

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,14 +3,13 @@
 echo "Installing Ambianic dependencies"
 
 # Install gstreamer
-sudo apt-get update && apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good python3-gst-1.0 python3-gi
+sudo apt-get update && sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good python3-gst-1.0 python3-gi
 
 # [backend]
 
 # make sure python sees the packages installed via apt-get
 # export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages
 echo "export PYTHONPATH=$PYTHONPATH:/usr/lib/python3/dist-packages" >> $HOME/.bashrc
-
 
 # Install Raspberry Pi video drivers
 if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
@@ -40,11 +39,13 @@ rm -f all_models.tar.gz
 # [frontend]
 
 # install latest npm
+cd ambianic/webapp/client
 sudo apt-get install -y npm
 # sudo npm install -g npm@latest
 # sudo npm install node
-npm install -g @vue/cli
-npm install -g parcel-bundler
+sudo npm install -g @vue/cli
+sudo npm install -g parcel-bundler
+cd ../../../
 
 # install local npm dependencies from package.json
 # runtime dir is where the executable code resides
