da5ce5286f40ed96a60d151d80ae418ad5fc79a9
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-09-19 14:35:50 -0500

    docs infrastructure scipts

---
+++ docs/build-docs.sh
@@ -0,0 +1,13 @@
+
+# exit on error, verbose mode
+set -ex
+
+# script's own dir
+MY_DIR="$(dirname "$0")"
+cd $MY_DIR/../
+echo PWD=$PWD
+pip3 install mkdocs
+pip3  install pydoc-markdown
+cp ambianic/webapp/client/public/favicon.ico $MY_DIR/docs/
+pydocmd simple ambianic++ ambianic.pipeline++ ambianic.webapp++ > "$MY_DIR/docs/ambianic-python-api.md"
+mkdocs build -f "$MY_DIR/mkdocs.yml"
