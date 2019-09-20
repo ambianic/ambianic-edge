d1ae76fb6283620b164733fcb83bbe6096148f36
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-09-20 14:24:25 -0500

    added code coverage and linting

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,6 +3,9 @@
 set -ex
 
 pip3 install -U pytest # unit test tool
+pip3 install -U codecov # code coverage tool
+pip3 install -U pytest-cov # coverage plugin for pytest
+pip3 install pylint # python linter
 BASEDIR=$(dirname $0)
 pip3 install -e $BASEDIR/../src
 echo "Script location: ${BASEDIR}"
