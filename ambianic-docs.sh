d2055a6ca8084c7229ad63e514e1618b420befff
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-07 11:12:30 -0500

    docs: fix build

--- ambianic-docs.sh
+++ ambianic-docs.sh
@@ -3,10 +3,20 @@
 # exit on error , verbose mode
 set -ex
 
+# detect effective CPU architecture
+if $(arch | grep -q 86)
+then
+  TAG="dev-x86"
+elif $(arch | grep -q arm)
+then
+  TAG="dev-arm"
+fi
+
+
 docker run -it --rm \
   --name ambianic-dev \
   --mount type=bind,source="$(pwd)",target=/workspace \
   --publish 1234:1234 \
   --publish 8778:8778 \
   --entrypoint 'bash' \
-  ambianic/ambianic:dev /workspace/docs/build-docs.sh
+  ambianic/ambianic::$TAG /workspace/docs/build-docs.sh
