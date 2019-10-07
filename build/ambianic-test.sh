42485fde2de5d3bc8e2fdb99dbb5f19d29e495ad
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-07 11:37:13 -0500

    build: fix

--- build/ambianic-test.sh
+++ build/ambianic-test.sh
@@ -13,7 +13,7 @@ fi
 
 docker run -it --rm \
   --name ambianic-dev \
-  --mount type=bind,source="$(pwd)",target=/workspace \
+  --mount type=bind,source="$(pwd)/../",target=/workspace \
   --publish 1234:1234 \
   --publish 8778:8778 \
   --entrypoint 'bash' \
