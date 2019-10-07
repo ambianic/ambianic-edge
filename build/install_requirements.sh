cad9d5b4e4304b8540a72841924adb76504b9837
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-07 17:58:57 -0500

    build: ARM script fix

--- build/install_requirements.sh
+++ build/install_requirements.sh
@@ -33,7 +33,9 @@ sudo apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base \
   gstreamer1.0-libav gstreamer1.0-doc \
   gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 \
   gstreamer1.0-pulseaudio \
-  python3-gst-1.0 python3-gi \
+  python3-gst-1.0 python3-gi
+# install numpy native lib
+sudo apt-get install -y python3-numpy
 
 
 # [backend]
