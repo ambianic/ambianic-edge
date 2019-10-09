3f5a6b23807be2633a7a4b0ec713791c2bb9ea50
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-10-09 00:13:35 -0500

    docs: more content

--- ambianic-start.sh
+++ ambianic-start.sh
@@ -3,10 +3,23 @@
 # docker run -it ambianic/ambianic bash
 # python3 ambianic.py
 # --rm : remove docker container on exit
+
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR=$(dirname "${MY_PATH}")
+
+# test if coral usb stick is available
+USB_DIR=/dev/bus/usb
+
+if [ -d "$DIRECTORY" ]; then
+  USB_ARG="--device $USB_DIR"
+else
+  USB_ARG=""
+fi
+
 docker run -it --rm \
   --name ambianic-dev \
-  --mount type=bind,source="$(pwd)",target=/workspace \
+  --mount type=bind,source="$MY_DIR",target=/workspace \
   --publish 1234:1234 \
   --publish 8778:8778 \
-  --device /dev/bus/usb \
+  $USB_ARG \
   ambianic/ambianic:dev
--- build/ambianic-test.sh
+++ build/ambianic-test.sh
@@ -11,9 +11,12 @@ then
   TAG="dev"
 fi
 
+MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
+MY_DIR=$(dirname "${MY_PATH}")
+
 docker run -it --rm \
   --name ambianic-dev \
-  --mount type=bind,source="$(pwd)/../",target=/workspace \
+  --mount type=bind,source="$MY_DIR/../",target=/workspace \
   --publish 1234:1234 \
   --publish 8778:8778 \
   --entrypoint 'bash' \
