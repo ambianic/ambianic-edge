a2d9b8c4586185f9ace9cebf0cf8ac4517fb1979
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-11-04 14:54:32 -0600

    build: build and publish production docker image

--- src/setup.py
+++ src/setup.py
@@ -1,21 +1,33 @@
+"""Setup for the Ambianic Edge package."""
 import setuptools
 
 with open("../README.md", "r") as fh:
     long_description = fh.read()
 
+version="0.11.4"
+
 setuptools.setup(
     name="ambianic",
-    version="0.0.11",
+    version=version,
     author="Ivelin Ivanov",
-    author_email="ivelin117@ambianic.ai",
-    description="AI framework for home and business automation",
+    author_email="ivelin.ivanov@ambianic.ai",
+    description="Edge component of the AI platform "
+                "for home and business automation",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://ambianic.ai",
+    license="Apache Software License 2.0",
     packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[],
     classifiers=[
+        'Development Status :: Alpha ' + version,
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache 2.0",
+        "OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        'TOPIC :: HOME AUTOMATION',
+        'TOPIC :: SCIENTIFIC/ENGINEERING :: ARTIFICIAL INTELLIGENCE'
+        'TOPIC :: SOFTWARE DEVELOPMENT :: EMBEDDED SYSTEMS',
+        'Intended Audience :: Developers',
     ],
 )
