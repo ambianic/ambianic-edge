3e3ff0a126aad9b6abc1a69f859b85ff2e74afdd
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-11-04 16:59:43 -0600

    build: use braces when expanding arrays

--- build/prepare-release.sh
+++ build/prepare-release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 VERSION=$1
 a=( ${VERSION//./ } )
-MAJOR=$a[0]
-MINOR=$a[1]
-PATCH=$a[2]
+MAJOR=${a[0]}
+MINOR=${a[1]}
+PATCH=${a[2]}
 $ docker tag ambianic/ambianic:latest ambianic/ambianic:$MAJOR
 $ docker push ambianic/ambianic:$MAJOR
 $ docker tag ambianic/ambianic:latest ambianic/ambianic:"$MAJOR.$MINOR"
