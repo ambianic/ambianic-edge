8e20202a57b36b5b5c1414fe90c07d1eb6bb8b02
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-11-11 11:00:09 -0600

    build: sem rel fix

--- build/prepare-release.sh
+++ build/prepare-release.sh
@@ -11,11 +11,11 @@ ls -al
 
 # push dev image
 docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
-docker push ambianic/ambianic-edge:${DEV_TAG}
-docker manifest create ambianic/ambianic-edge:dev ambianic/ambianic-edge:dev-amd64 ambianic/ambianic-edge:dev-arm32v7
-docker manifest annotate ambianic/ambianic-edge:dev ambianic/ambianic-edge:dev-arm32v7 --os=linux --arch=arm --variant=v7
-docker manifest annotate ambianic/ambianic-edge:dev ambianic/ambianic-edge:dev-amd64 --os=linux --arch=amd64
-docker manifest push ambianic/ambianic-edge:dev
+docker push "ambianic/ambianic-edge:${DEV_TAG}"
+docker manifest create "ambianic/ambianic-edge:dev" "ambianic/ambianic-edge:dev-amd64" "ambianic/ambianic-edge:dev-arm32v7"
+docker manifest annotate "ambianic/ambianic-edge:dev" "ambianic/ambianic-edge:dev-arm32v7" --os=linux --arch=arm --variant=v7
+docker manifest annotate "ambianic/ambianic-edge:dev" "ambianic/ambianic-edge:dev-amd64" --os=linux --arch=amd64
+docker manifest push "ambianic/ambianic-edge:dev"
 
 # push prod image
 cd ${TRAVIS_BUILD_DIR}
@@ -25,11 +25,11 @@ ls -al
 cp README.md ./src
 
 docker build -f ./build/Prod.Dockerfile --platform ${ARCH} -t "ambianic/ambianic-edge:${PROD_TAG}" ./src
-docker tag ambianic/ambianic-edge:${PROD_TAG} ambianic/ambianic-edge:latest
-docker manifest create ambianic/ambianic-edge:latest ambianic/ambianic-edge:latest-amd64 ambianic/ambianic-edge:latest-arm32v7
-docker manifest annotate ambianic/ambianic-edge:latest ambianic/ambianic-edge:latest-arm32v7 --os=linux --arch=arm --variant=v7
-docker manifest annotate ambianic/ambianic-edge:latest ambianic/ambianic-edge:latest-amd64 --os=linux --arch=amd64
-docker manifest push ambianic/ambianic-edge:latest
+docker tag "ambianic/ambianic-edge:${PROD_TAG}" "ambianic/ambianic-edge:latest"
+docker manifest create "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:latest-amd64" "ambianic/ambianic-edge:latest-arm32v7"
+docker manifest annotate "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:latest-arm32v7" --os=linux --arch=arm --variant=v7
+docker manifest annotate "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:latest-amd64" --os=linux --arch=amd64
+docker manifest push "ambianic/ambianic-edge:latest"
 RELEASE_VERSION=$1
 a=( ${RELEASE_VERSION//./ } )
 MAJOR=${a[0]}
@@ -39,9 +39,9 @@ echo "RELEASE_VERSION=$RELEASE_VERSION"
 echo "MAJOR=$MAJOR"
 echo "MINOR=$MAJOR.$MINOR"
 echo "PATCH=$PATCH"
-docker tag ambianic/ambianic-edge:latest ambianic/ambianic-edge:$MAJOR
-docker push ambianic/ambianic-edge:$MAJOR
-docker tag ambianic/ambianic-edge:latest ambianic/ambianic-edge:"$MAJOR.$MINOR"
-docker push ambianic/ambianic-edge:"$MAJOR.$MINOR"
-docker tag ambianic/ambianic-edge:latest ambianic/ambianic-edge $RELEASE_VERSION
-docker push ambianic/ambianic-edge:$RELEASE_VERSION
+docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:$MAJOR"
+docker push "ambianic/ambianic-edge:$MAJOR"
+docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:$MAJOR.$MINOR"
+docker push "ambianic/ambianic-edge:$MAJOR.$MINOR"
+docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge $RELEASE_VERSION"
+docker push "ambianic/ambianic-edge:$RELEASE_VERSION"
