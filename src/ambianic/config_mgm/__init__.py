1aea90896c2f76a61322d49080c2f58c5262bf50
Ivelin Ivanov <ivelin@ambianic.ai>
2020-06-22 12:51:45 -0500

    fix: redundant import of ConfigChangedEvent

--- src/ambianic/config_mgm/__init__.py
+++ src/ambianic/config_mgm/__init__.py
@@ -1,3 +1,3 @@
 
-from .config_diff import ConfigChangedEvent, Config, ConfigList, ConfigDict, ConfigChangedEvent
+from .config_diff import ConfigChangedEvent, Config, ConfigList, ConfigDict
 from .configuration_manager import ConfigurationManager
