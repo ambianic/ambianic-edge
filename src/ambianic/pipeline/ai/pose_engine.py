cd82091f6a887c45dd52013ecf145d97c598eb51
Will Naylor <30575955+will300@users.noreply.github.com>
2020-08-13 09:58:02 +0100

    Update pose_engine.py

--- src/ambianic/pipeline/ai/pose_engine.py
+++ src/ambianic/pipeline/ai/pose_engine.py
@@ -12,18 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import collections
-import math
-
-import numpy as np
-from pkg_resources import parse_version
-from edgetpu import __version__ as edgetpu_version
-assert parse_version(edgetpu_version) >= parse_version('2.11.1'), \
-        'This demo requires Edge TPU version >= 2.11.1'
-
 from edgetpu.basic.basic_engine import BasicEngine
 from edgetpu.utils import image_processing
 from PIL import Image
+from pkg_resources import parse_version
+from edgetpu import __version__ as edgetpu_version
+
+import collections
+import math
+import numpy as np
+
+assert parse_version(edgetpu_version) >= parse_version('2.11.1'), \
+        'This demo requires Edge TPU version >= 2.11.1'
 
 KEYPOINTS = (
   'nose',
@@ -44,6 +44,7 @@ KEYPOINTS = (
   'left ankle',
   'right ankle'
 )
+
 
 class Keypoint:
     __slots__ = ['k', 'yx', 'score']
@@ -90,7 +91,8 @@ class PoseEngine(BasicEngine):
             raise ValueError(
                 ('Image model should have input shape [1, height, width, 3]!'
                  ' This model has {}.'.format(self._input_tensor_shape)))
-        _, self.image_height, self.image_width, self.image_depth = self.get_input_tensor_shape()
+        _, self.image_height, self.image_width, self.image_depth = \
+                                                self.get_input_tensor_shape()
 
         # The API returns all the output tensors flattened and concatenated. We
         # have to figure out the boundaries from the tensor shapes & sizes.
@@ -101,19 +103,21 @@ class PoseEngine(BasicEngine):
             self._output_offsets.append(int(offset))
 
     def DetectPosesInImage(self, img):
-        """Detects poses in a given image.
-           For ideal results make sure the image fed to this function is close to the
-           expected input size - it is the caller's responsibility to resize the
-           image accordingly.
+        """
+        Detects poses in a given image.
+        For ideal results make sure the image fed to this function is 
+        close to the expected input size - it is the caller's 
+        responsibility to resize the image accordingly.
         Args:
           img: numpy array containing image
         """
 
         # Extend or crop the input to match the input shape of the network.
-        if img.shape[0] < self.image_height or img.shape[1] < self.image_width:
+        if (img.shape[0] < self.image_height) or \
+           (img.shape[1] < self.image_width):
             img = np.pad(img, [[0, max(0, self.image_height - img.shape[0])],
-                               [0, max(0, self.image_width - img.shape[1])], [0, 0]],
-                         mode='constant')
+                               [0, max(0, self.image_width - img.shape[1])], 
+                               [0, 0]], mode='constant')
         img = img[0:self.image_height, 0:self.image_width]
         assert (img.shape == tuple(self._input_tensor_shape[1:]))
 
@@ -122,7 +126,8 @@ class PoseEngine(BasicEngine):
 
     def ParseOutput(self, output):
         inference_time, output = output
-        outputs = [output[i:j] for i, j in zip(self._output_offsets, self._output_offsets[1:])]
+        outputs = [output[i:j] for i, j in zip(self._output_offsets, \
+                                               self._output_offsets[1:])]
 
         keypoints = outputs[0].reshape(-1, len(KEYPOINTS), 2)
         keypoint_scores = outputs[1].reshape(-1, len(KEYPOINTS))
@@ -130,15 +135,16 @@ class PoseEngine(BasicEngine):
         nposes = int(outputs[3][0])
         assert nposes < outputs[0].shape[0]
 
-        # Convert the poses to a friendlier format of keypoints with associated
-        # scores.
+        # Convert the poses to a friendlier format 
+        # of keypoints with associated scores.
         poses = []
         for pose_i in range(nposes):
             keypoint_dict = {}
             for point_i, point in enumerate(keypoints[pose_i]):
                 keypoint = Keypoint(KEYPOINTS[point_i], point,
                                     keypoint_scores[pose_i, point_i])
-                if self._mirror: keypoint.yx[1] = self.image_width - keypoint.yx[1]
+                if self._mirror: 
+                    keypoint.yx[1] = self.image_width - keypoint.yx[1]
                 keypoint_dict[KEYPOINTS[point_i]] = keypoint
             poses.append(Pose(keypoint_dict, pose_scores[pose_i]))
 
