e834ddaffc0d8665bd85a4b9c1f1894ad1e441d4
ivelin <ivelin@ambianic.ai>
2020-12-11 21:26:29 +0000

    dix: ci

--- build/ambianic-test.sh
+++ build/ambianic-test.sh
@@ -14,7 +14,7 @@ fi
 MY_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_SOURCE[0]}")"
 MY_DIR=$(dirname "${MY_PATH}")
 
-docker run -it --rm \
+docker run --rm \
   --name ambianic-dev \
   --mount type=bind,source="$MY_DIR/../",target=/workspace \
   --net=host \
