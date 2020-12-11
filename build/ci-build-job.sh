979ce4a247553188b22023b7b0094586e919e461
ivelin <ivelin@ambianic.ai>
2020-12-11 21:09:06 +0000

    fix: ci

--- build/ci-build-job.sh
+++ build/ci-build-job.sh
@@ -3,6 +3,8 @@
 # set bash debug parameters
 set -exu
 
+echo "Building for CPU architecture: ${ARCH}"
+
 # Prepare docker engine enviroment
 sudo apt-get update -y
 # sudo apt-get install --only-upgrade docker-ce -y
@@ -14,8 +16,8 @@ docker --version
 
 # Raspberry PI section
 # prepare qemu
-DEV_TAG=dev-${ARCH}
-PROD_TAG=latest-${ARCH}
+DEV_TAG=dev-${TAG_SUFFIX}
+PROD_TAG=latest-${TAG_SUFFIX}
 docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d
 cat /proc/sys/fs/binfmt_misc/qemu-aarch64
 cd ${GITHUB_WORKSPACE}/build
