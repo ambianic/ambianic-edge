b061c335ae0ca6a42ad2abfd03c7bea303c15e43
ivelin <ivelin@ambianic.ai>
2020-12-16 04:29:41 +0000

    fix: codecov upload

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,16 +3,16 @@
 set -ex
 
 # parse command line arguments
-while getopts ":u" flag
-do
-    case "${flag}" in
-        u) upload_codecov=true;;
-    esac
-done
+# while getopts ":u" flag
+# do
+#     case "${flag}" in
+#        u) upload_codecov=true;;
+#     esac
+# done
 
-if [ "$upload_codecov" = true ] ; then
-    echo 'Code coverage upload flag set. Will upload report to codecov.io'
-fi
+# if [ "$upload_codecov" = true ] ; then
+#     echo 'Code coverage upload flag set. Will upload report to codecov.io'
+# fi
 
 pip3 install -U pytest # unit test tool
 pip3 install -U codecov # code coverage tool
@@ -30,8 +30,8 @@ python3 -m pytest --cov=ambianic --cov-report=xml --cov-report=term tests/
 
 # if -u command line argument is passed, submit code coverage report to codecov.io
 # parse command line arguments
-if [ "$upload_codecov" = true ] ; then
-    echo 'Uploading code coverage report to codecov.io'
-    codecov
-fi
+# if [ "$upload_codecov" = true ] ; then
+#   echo 'Uploading code coverage report to codecov.io'
+#    codecov
+# fi
 
