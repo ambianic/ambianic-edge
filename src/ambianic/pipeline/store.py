10fe064a044fcbb0370314a39dd27347124b68b2
bhavikapanara <panara.bhavika@gmail.com>
2021-02-22 23:37:26 +0530

    update

--- src/ambianic/pipeline/store.py
+++ src/ambianic/pipeline/store.py
@@ -185,9 +185,9 @@ class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, np.integer):
             return int(obj)
-        elif isinstance(obj, np.floating):
+        if isinstance(obj, np.floating):
             return float(obj)
-        elif isinstance(obj, np.ndarray):
+        if isinstance(obj, np.ndarray):
             return obj.tolist()
-        else:
-            return super(JsonEncoder, self).default(obj)
+
+        return super(JsonEncoder, self).default(obj)
