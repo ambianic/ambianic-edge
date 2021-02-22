c2129893099f39cbdfa65e2d772d6b032511c79e
bhavikapanara <panara.bhavika@gmail.com>
2021-02-22 16:52:05 +0530

    add test cases for jsonEncoder

--- tests/pipeline/test_store.py
+++ tests/pipeline/test_store.py
@@ -1,12 +1,51 @@
 """Test cases for SaveDetectionSamples."""
 
-from ambianic.pipeline.store import SaveDetectionSamples
+from json import encoder
+from ambianic.pipeline.store import SaveDetectionSamples, JsonEncoder
 from PIL import Image
 import numpy as np
 import os
 import json
 import logging
 from ambianic.pipeline.timeline import PipelineContext
+
+
+def test_json_encoder():
+
+    inp = {
+            'label': 'FALL',
+            'confidence': np.float32(12.3),
+            'leaning_angle': np.float32(24.3),
+            'keypoint_corr': {
+                'left shoulder': [np.float32(1.2), np.float32(1.23)],
+                'left hip': [np.float32(1.2), np.float32(1.23)],
+                'right shoulder': [np.float32(1.2), np.float32(1.23)],
+                'right hip': [np.float32(1.2), np.float32(1.23)]
+            }
+        }
+    encode = json.dumps(inp, cls=JsonEncoder)
+    decode = json.loads(encode)
+
+    assert type(decode['confidence']) == float
+    assert type(decode['leaning_angle']) == float
+    assert type(decode['keypoint_corr']['left shoulder'][0]) == float
+    assert type(decode['keypoint_corr']['left shoulder'][1]) == float
+
+
+def test_json_encoder_integerData():
+    input = np.int32(10)
+    encode = json.dumps(input, cls=JsonEncoder)
+    decode = json.loads(encode)
+
+    assert type(decode) == int
+
+
+def test_json_encoder_arrayData():
+    input = np.array([1, 2, 3, 4, 5])
+    encode = json.dumps(input, cls=JsonEncoder)
+    decode = json.loads(encode)
+
+    assert type(decode) == list
 
 
 def test_process_sample_none():
@@ -18,41 +57,83 @@ def test_process_sample_none():
     assert len(processed_samples) == 1
     assert processed_samples[0] is None
 
-# def test_process_sample():
-#     store = SaveDetectionSamples()      
-#     img = Image.new('RGB', (60, 30), color='red')
 
-#     detections = [
-#                     {
-#                      'label': 'person',
-#                      'confidence': np.float32(0.98),
-#                      'box': {
-#                          'xmin': np.float32(0),
-#                          'ymin': np.float32(1),
-#                          'xmax': np.float32(2),
-#                          'ymax': np.float32(3)
-#                         }
-#                     }
-#                 ]
+def test_process_sample():
 
-#     processed_samples = list(store.process_sample(image=img,
-#                                                   thumbnail=img,
-#                                                   inference_result=detections,
-#                                                   inference_meta=None))
-    
-#     assert len(processed_samples) == 1
-#     print(processed_samples)
-#     inf = processed_samples[0]['inference_result']
-#     print(inf)
+    out_dir = os.path.dirname(os.path.abspath(__file__))
+    out_dir = os.path.join(
+        out_dir,
+        'tmp/'
+        )
+    out_dir = os.path.abspath(out_dir)
+    context = PipelineContext(unique_pipeline_name='test pipeline')
+    context.data_dir = out_dir
+    store = _TestSaveDetectionSamples(context=context,
+                                      event_log=logging.getLogger())
+    img = Image.new('RGB', (60, 30), color='red')
 
-#     category = inf[0]['label']
-#     confidence = inf[0]['confidence']
-#     (x0, y0) = inf[0]['box']['xmin'], inf[0]['box']['ymin']
-#     (x1, y1) = inf[0]['box']['xmax'], inf[0]['box']['ymax']
+    detections = [
+                    {
+                     'label': 'person',
+                     'confidence': np.float32(0.98),
+                     'box': {
+                         'xmin': np.float32(0.1),
+                         'ymin': np.float32(1.1),
+                         'xmax': np.float32(2.1),
+                         'ymax': np.float32(3.1)
+                        }
+                    }
+                ]
 
-#     assert category == 'person'
-#     assert confidence == 0.98
-#     assert x0 == 0 and y0 == 1 and x1 == 2 and y1 == 3
+    processed_samples = list(store.process_sample(image=img,
+                                                  thumbnail=img,
+                                                  inference_result=detections))
+    assert len(processed_samples) == 1
+    print(processed_samples)
+    img_out = processed_samples[0]['image']
+    assert img_out == img
+    inf = processed_samples[0]['inference_result']
+    print(inf)
+
+    category = inf[0]['label']
+    confidence = inf[0]['confidence']
+    (x0, y0) = inf[0]['box']['xmin'], inf[0]['box']['ymin']
+    (x1, y1) = inf[0]['box']['xmax'], inf[0]['box']['ymax']
+
+    assert category == 'person'
+    assert type(confidence) == np.float32
+    assert type(x0) == type(y0) == type(x1) == type(y1) == np.float32
+    assert store._save_sample_called
+    assert store._inf_result == detections
+    assert store._img_path
+    img_dir = os.path.dirname(os.path.abspath(store._img_path / "../../"))
+    assert img_dir == out_dir
+    out_img = Image.open(store._img_path)
+    print(img_dir)
+    print(store._img_path)
+    assert out_img.mode == 'RGB'
+    assert out_img.size[0] == 60
+    assert out_img.size[1] == 30
+    json_dir = os.path.dirname(os.path.abspath(store._json_path / "../../"))
+    assert json_dir == out_dir
+    print(json_dir)
+    print(store._json_path)
+    with open(store._json_path) as f:
+        json_inf = json.load(f)
+        print(json_inf)
+        img_fname = json_inf['image_file_name']
+        rel_dir = json_inf['rel_dir']
+        img_fpath = os.path.join(out_dir, rel_dir, img_fname)
+        assert img_fpath == str(store._img_path)
+        json_inf_res = json_inf['inference_result']
+        assert len(json_inf_res) == 1
+        json_inf_res = json_inf_res[0]
+        assert json_inf_res['label'] == 'person'
+        assert type(json_inf_res['confidence']) == float
+        assert type(json_inf_res['box']['xmin']) == float
+        assert type(json_inf_res['box']['ymin']) == float
+        assert type(json_inf_res['box']['xmax']) == float
+        assert type(json_inf_res['box']['ymax']) == float
 
 
 class _TestSaveDetectionSamples(SaveDetectionSamples):
