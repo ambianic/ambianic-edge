622537a3f886e4c52653d7cb36c085c220c17a6f
Ivelin Ivanov <ivelin@ambianic.ai>
2021-08-18 17:23:08 -0500

    build: fix release versioning (#365)

--- build/prepare-semantic-release.sh
+++ build/prepare-semantic-release.sh
@@ -13,11 +13,15 @@ echo "MAJOR=$MAJOR"
 echo "MINOR=$MAJOR.$MINOR"
 echo "PATCH=$PATCH"
 
+# Which dir are we in?
+pwd
+
 # update version info in the ambianic python package setup.cfg
 cp README.md src/
 cd src
 python3 setup.py setopt --command metadata --option version --set-value $RELEASE_VERSION
-
+# verify if version is updated in setup.cfg
+cat setup.cfg
 
 # update docker to allow multi-arch manifest support
 sudo apt-get update -y
@@ -27,15 +31,30 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 sudo service docker restart
 docker --version
 
-docker pull "ambianic/ambianic-edge:latest"
+###
+# Build docker images for arm32 and amd architectures
+###
 
-# create versioned image tags
-# docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:$MAJOR"
-# docker push "ambianic/ambianic-edge:$MAJOR"
-# docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:$MAJOR.$MINOR"
-# docker push "ambianic/ambianic-edge:$MAJOR.$MINOR"
-# docker tag "ambianic/ambianic-edge:latest" "ambianic/ambianic-edge:$RELEASE_VERSION"
-# docker push "ambianic/ambianic-edge:$RELEASE_VERSION"
+export ARCH="linux/amd64"
+export TAG_SUFFIX="amd64"
+pwd
+echo GITHUB_WORKSPACE=${GITHUB_WORKSPACE}
+echo "ARCH=${ARCH}"
+echo "TAG_SUFFIX=${TAG_SUFFIX}"
+${GITHUB_WORKSPACE}/build/ci-prep-release-job.sh
+
+export ARCH: "linux/arm/v7"
+export TAG_SUFFIX: "arm32v7"
+pwd
+echo GITHUB_WORKSPACE=${GITHUB_WORKSPACE}
+echo "ARCH=${ARCH}"
+echo "TAG_SUFFIX=${TAG_SUFFIX}"
+${GITHUB_WORKSPACE}/build/ci-prep-release-job.sh
+
+####
+# Docker image versioning section
+####
+docker pull "ambianic/ambianic-edge:latest"
 
 # create multi-architecture manifests for each version
 docker manifest create "ambianic/ambianic-edge:$MAJOR" "ambianic/ambianic-edge:latest-amd64" "ambianic/ambianic-edge:latest-arm32v7"
@@ -47,7 +66,6 @@ docker manifest create "ambianic/ambianic-edge:$MAJOR.$MINOR" "ambianic/ambianic
 docker manifest annotate "ambianic/ambianic-edge:$MAJOR.$MINOR" "ambianic/ambianic-edge:latest-arm32v7" --os=linux --arch=arm --variant=v7
 docker manifest annotate "ambianic/ambianic-edge:$MAJOR.$MINOR" "ambianic/ambianic-edge:latest-amd64" --os=linux --arch=amd64
 docker manifest push "ambianic/ambianic-edge:$MAJOR.$MINOR"
-
 
 docker manifest create "ambianic/ambianic-edge:$RELEASE_VERSION" "ambianic/ambianic-edge:latest-amd64" "ambianic/ambianic-edge:latest-arm32v7"
 docker manifest annotate "ambianic/ambianic-edge:$RELEASE_VERSION" "ambianic/ambianic-edge:latest-arm32v7" --os=linux --arch=arm --variant=v7
